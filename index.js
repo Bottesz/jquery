ff3125938a1d84f1af8493241c4a645f583b77f3
Bottesz <botyaring@gmail.com>
2024-03-21 09:08:20 +0100

    jquery-tanulasa

---
+++ index.js
@@ -0,0 +1,11 @@
+/*window.addEventListener("load",function(){
+    const pELEM=document.querySelector("p")
+    pELEM.innerHTML="átírtam a bekezdést"
+
+})
+*/
+
+/*Fogjuk meg a .kivalaszt div-et*/
+
+const ELEM=$(".kivalaszt")
+ELEM.html("<h2> Hello JQUERY! </h2>")
